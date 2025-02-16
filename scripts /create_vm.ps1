fa70d42cb925ecc2dea758819187a5ffd59f8846
Lillian McGowan <lmcgowan92@gmail.com>
2025-02-16 14:43:27 -0600

    Update and rename scripts to scripts /create_vm.ps1

---
+++ scripts /create_vm.ps1
@@ -0,0 +1,5 @@
+Enable Auto-Shutdown to Reduce Costs
+
+This will schedule the VM to shut down at 10:00 PM daily to save cloud costs:
+Set-AzVM -ResourceGroupName "VM-Group" -Name "EnterpriseVM" -AutoShutdown 22:00
+
